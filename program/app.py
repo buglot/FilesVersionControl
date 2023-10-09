bd7d25f87e94bf35b1d894254b2bee62d3006c5c
Buglot <65818720+buglot@users.noreply.github.com>
2023-10-09 19:23:08 +0700

    now

--- program/app.py
+++ program/app.py
@@ -1,58 +1,92 @@
 from ftplib import FTP
 from printColor import *
 from dir import * 
+import threading
 import os
 import time
-ftp = FTP("10.64.59.68")
+
+ftp = FTP(input("IP FTP: "))
+if d:=input("Port (n is skip):")=="n":
+    pass
+else:
+    ftp.port = d
 ftp.login(user="myuser", passwd="mypassword")
 printHeader(" Connected Server ")
+def upload(files,x,name):
+        global ftp
+        named_tuple = time.localtime() 
+        time_string = time.strftime("%H-%M-%S", named_tuple)
+        
+        remote_file_name = x+"_name=="+name+time_string
+        with open(files, "rb") as local_file:
+            ftp.storbinary("STOR " + remote_file_name, local_file)
 def printFILEcloud():
     filenames = ftp.nlst()
     for f in filenames:
-        if not is_file(ftp,f):
+        if not is_file(ftp,f):                
             printSubDir(ftp.nlst(f))
         else:
             printFile(f)
 def donwload(files,local =""):
     global ftp
     if local == "":
-       
         local= os.path.join("save",files)
     if os.path.exists("save")==False:
-            os.mkdir("save")
+        os.mkdir("save")
     with open(local, "wb") as local_file:
         ftp.retrbinary("RETR " + files, local_file.write)
-def upload(files,x,name):
-    global ftp
-    named_tuple = time.localtime() 
-    time_string = time.strftime("%H-%M-%S", named_tuple)
-    
-    remote_file_name = x+"_name=="+name+time_string
-    with open(files, "rb") as local_file:
-        ftp.storbinary("STOR " + remote_file_name, local_file)
-print()
-printDir("\ Could")
-printFILEcloud()
-name = input("Name : ")
-while 1:
-    commend = input("promp >> ")
-    match(commend.split(" ")[0]):
-        case "download":
-            if len(commend.split(" "))==2:
-                donwload(commend.split(" ")[1])
-            else:
-                print("Error")
-        case "put":
-            if os.path.exists("save")==False:
-                print("no Folder to save")
-            elif os.listdir() is []:
-                print("not file put")
-            else:
-                for x in os.listdir("save"):
-                    
+        local_file.close()
+    times = os.path.getatime(local)
+    with open(os.path.join(".p",local),"wb") as f:
+        f.write(times)
+        f.close() 
+def dectecing():
+    while(1):
+        if os.path.exists("save")==False:
+            os.mkdir("save")
+        elif os.listdir() is []:
+            pass
+        else:
+            for x in os.listdir("save"):
+                if os.path.exists(os.path.join(".p",x))==True:
+                    if history(x)< os.path.getmtime(os.path.join("save",x)):
+                        upload(os.path.join("save",x),x,name)
+                        with open(os.path.join(".p",x),"w") as f:
+                            f.write(str(os.path.getmtime(os.path.join("save",x))))
+                            f.close()
+                else:
+                    with open(os.path.join(".p",x),"w") as f:
+                        f.write(str(os.path.getmtime(os.path.join("save",x))))
+                        f.close()
                     upload(os.path.join("save",x),x,name)
-        case "exit":
-            exit()
-ftp.quit()
-
-
+def history(files):
+    timee = 0
+    histo=os.path.join(".p",files)
+    with open(histo) as f:
+        timee =f.readline()
+    
+    return float(timee)
+if __name__ == "__main__":
+    if os.path.exists(".p")==False:
+        os.makedirs(".p")
+    print()
+    printDir("\ Could")
+    printFILEcloud()
+    name = input("Name : ")
+    monitor_thread = threading.Thread(target=dectecing)
+    monitor_thread.start()
+    while 1:
+        commend = input("promp >> ")
+        match(commend.split(" ")[0]):
+            case "download":
+                if len(commend.split(" "))==2:
+                    donwload(commend.split(" ")[1])
+                elif len(commend.split(" "))==3:
+                    donwload(commend.split(" ")[1],commend.split(" ")[2])
+                else:
+                    print("Error")
+            case "exit":
+                exit()
+    
+    monitor_thread.join()
+    ftp.quit()
